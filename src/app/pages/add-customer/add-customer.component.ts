7882afeb9fa5e1a5e72b2298dd8c6a2d4d053c48
Lithira-Dilitha <lithiradilitha2022@gmail.com>
2024-07-26 15:49:29 +0530

    add Customer Function

---
+++ src/app/pages/add-customer/add-customer.component.ts
@@ -0,0 +1,35 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import Swal from 'sweetalert2';
+
+@Component({
+  selector: 'app-add-customer',
+  standalone: true,
+  imports: [FormsModule,HttpClientModule,CommonModule],
+  templateUrl: './add-customer.component.html',
+  styleUrl: './add-customer.component.css'
+})
+
+export class AddCustomerComponent {
+  constructor(private http:HttpClient){}
+  public customerobj ={
+    "id":"",
+    "name":"",
+    "city":"",
+    "contact":""
+}
+  addCustomer(){
+    this.http.post("http://localhost:8080/customer-controller/customer",this.customerobj).subscribe(
+      (data)=>{
+        Swal.fire({
+          title: "Customer Add",
+          text: "You clicked the button!",
+          icon: "success"
+        });
+      }
+    )
+  }
+}
+
